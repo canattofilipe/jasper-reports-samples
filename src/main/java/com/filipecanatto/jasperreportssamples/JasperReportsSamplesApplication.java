10a02f4ab635e9417a5e7d0221244bd09a61b86e
Wilton Filipe Canatto <wilton@ciandt.com>
2021-09-20 10:26:58 -0300

    Add Spring Boot files

---
+++ src/main/java/com/filipecanatto/jasperreportssamples/JasperReportsSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.filipecanatto.jasperreportssamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperReportsSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportsSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipecanatto/jasperreportssamples/JasperReportsSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipecanatto.jasperreportssamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperReportsSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
